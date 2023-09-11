5a9bf725c66159bbfba65878ec8146bafbf65f57
Mahavir Patel <patelmahavir2003@outlook.com>
2023-09-11 09:16:20 +0530

    Practicals Upto 6(UI)

--- MAD_Practical_1_21012011088/src/main/kotlin/Pr_1_4.kt
+++
@@ -1,7 +0,0 @@
-//Que : Find the number is odd or even by using Control Flow inside println() method.
-
-fun main() {
-    print("Enter a Number : ")
-    val number = readln().toInt();
-    println("The number $number is ${if (number % 2 == 0) "even" else "odd"}.")
-}
